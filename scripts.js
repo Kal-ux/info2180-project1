e3a6af1325198ce7c614e196187318ad8315e6bd
Rashede Callum <rcallum461@gmail.com>
2021-10-23 22:31:46 -0500

    edited Javascript file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-/* Add your JavaScript to this file */
+window.onload = function(){
+
+    var myForm = document.querySelector("div form");
+
+    myForm.addEventListener('submit', e =>{
+        e.preventDefault();
+
+        var email = document.getElementById("email").value;
+
+        if(email != ""){
+            alert(`Thank you for subscribing! Your email address: ${email} has been added to our mailing list!`);
+        }else{
+            alert('Please enter a Valid Email Address');
+
+        }
+
+    })
+    
+}
